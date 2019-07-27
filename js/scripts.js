643fff6520d4dd08c46c0388da56fceebcc5ac6b
WendyOmollo <omollow280@gmail.com>
2019-07-27 17:53:50 +0300

    Make the toppings div hide and be shown when order button is clicked

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,21 @@
-$(document).ready(function(){
-
-$(".ordering").click(function(){
-  $(".details").slideDown();
-  $("#purchase").slideDown();
-});
-});
-
-function OrderPizza(size,toppings,quantity){
+// Business logic
+function OrderPizza(price,crust,toppings,quantity){
   this.size = price;
+  this.crust = crust;
   this.toppings = toppings;
   this.quantity = quantity;
 }
+// User interface
+// Displaying the ordering form
+$(document).ready(function(){
+  $(".ordering").click(function(){
+    $(".details").slideDown();
+    $("#purchase").slideDown();
+    $(".toppings").slideDown();
+  });
+  
+  });
+// Alerting the user that their order has been taken
 $(document).ready(function(){
   $("#purchase").click(function(){
   alert("We have received your order.Your pizza shall be delivered to you.Thank you for being a good customer.");

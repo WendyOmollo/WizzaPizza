3f33ff585ae132b3c3686891dfc261635b67a382
WendyOmollo <omollow280@gmail.com>
2019-07-27 12:58:30 +0300

    Add a constructor

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,15 @@
 $(document).ready(function(){
-  
+
 $(".ordering").click(function(){
   $(".details").show();
+  $("#purchase").show();
 });
+});
+
+function OrderPizza(size,toppings,quantity){
+  this.size = price;
+  this.toppings = toppings;
+  this.quantity = quantity;
+}
+$(document).ready(function(){
 });

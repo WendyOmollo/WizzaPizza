af0a9e7efc4e3f1a5b8f4f5fc8c4c7ccebb8f68a
WendyOmollo <omollow280@gmail.com>
2019-07-28 22:57:05 +0300

    Make a function respond
    It is responding as NaN still

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,3 @@
-// Business logic
-function OrderPizza(price,crust,toppings,quantity){
-  this.size = price;
-  this.crust = crust;
-  this.toppings = toppings;
-  this.quantity = quantity;
-}
 // User interface
 // Displaying the ordering form
 $(document).ready(function(){
@@ -13,15 +6,82 @@ $(document).ready(function(){
     $("#purchase").slideDown();
     $(".toppings").slideDown();
     $("#placeorder").slideDown();
-    
-  });
+
   $("#placeorder").click(function(){
-  $("#delivery").slideDown();
+      // Displays the delivery button
+        $("#delivery").slideDown();
   });
+});
+  // Sets the checked state of a checkbox
+  $(document).ready(function(){
+    var boxes = $(".custom-checkbox");
+    function checkBoxes(event){
+      document.body.div.span = event.target.value;
+      for(var i=0; i<boxes.length; i++){
+        boxes[i].addEventListener(checkBoxes);
+      }
+    }
+    // function check(){
+    //   $("#defaultInline1").checked = true;
+    //   $("#defaultInline2").checked = true;
+    //   $("#defaultInline3").checked = true;
+    //   $("#defaultInline4").checked = true;
+    //   $("#defaultInline5").checked = true;
+    //   $("#defaultInline6").checked = true;
+    // }
+    // function uncheck(){
+    //   $("#defaultInline1").checked = false;
+    //   $("#defaultInline2").checked = false;
+    //   $("#defaultInline3").checked = false;
+    //   $("#defaultInline4").checked = false;
+    //   $("#defaultInline5").checked = false;
+    //   $("#defaultInline6").checked = false;
+    // }
+  });
+   
+  $("#placeorder").click(function(event){
+      event.preventDefault();
+  // Gets the input of one pizza
+      var onePizza = function add(pizzaSize,pizzaCrust,pizzaTop){
+      var pizzaSize = parseInt($("#inputGroupSelect1 option:selected").val());
+      var pizzaCrust=parseInt($("#inputGroupSelect2 option:selected").val());
+      // var pizzaTop= parseInt($("checkbox:span").val());
+        return pizzaSize + pizzaCrust;
+      }
+  // Multiplies the total of one pizza times how many they are ordered
+       function multiply(){
+        var pizzaQuantity= parseInt($("input#amount").val());
+        return onePizza() * pizzaQuantity;
+      }
+      alert(multiply());
+
+      
+
+  });
+ 
+  ; 
   
   });
+
+
+
+
+
 // Alerting the user that their order has been taken
 $(document).ready(function(){
+  $("#delivery").click(function(){
+    function addDelivery(){
+      return multiply() + 150;
+    }
+    addDelivery()
+    prompt("Delivery fee is 150/=.Enter your location");
+    var location = ""
+    switch(location){
+      case " "
+    }
+
+
+  });
   $("#purchase").click(function(){
   alert("We have received your order.Your pizza shall be delivered to you.Thank you for being a good customer.");
   });

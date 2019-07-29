44d41aa3478e43bdb333683abc14191d51a72186
WendyOmollo <omollow280@gmail.com>
2019-07-29 12:56:05 +0300

    Add a statement to the alert box

--- js/scripts.js
+++ js/scripts.js
@@ -71,7 +71,7 @@ $(document).ready(function(){
          var pizzaQuantity= $("input#amount").val();
          return onePizza() * parseInt(pizzaQuantity);
        }
-       alert(multiply());
+       alert("Current price is " + multiply());
        $(".toDeliver").show();
  
 // Alerting the user that their order has been taken
@@ -81,10 +81,7 @@ $(document).ready(function(){
        function addDelivery(){
         return multiply() + 150;
     }
-        alert(addDelivery());
-        alert("Thank you for your working with us." + "Your pizza shall be delivered at " + deliveryPlace);
+        alert("Total price is" + addDelivery());
+        alert("Thank you for your working with us." + "Your pizza(s) shall be delivered at " + deliveryPlace);
   });
-
-  
-
 });
